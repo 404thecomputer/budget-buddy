05016d66982f498727e11ba7d92fd60e163444f4
Colten Berry <berrycj@hendrix.edu>
2024-09-22 20:42:11 -0500

    I have made a listView. it is loaded automatically first

---
+++ budget_buddy/lib/item_list_feature/item_list_view.dart
@@ -0,0 +1,42 @@
+import 'package:budget_buddy/objects/item.dart';
+import 'package:flutter/material.dart';
+
+class ItemListView extends StatefulWidget {
+  const ItemListView({
+    super.key,
+  });
+  @override
+  State<StatefulWidget> createState() {
+    return ItemListViewState();
+  }
+}
+
+class ItemListViewState extends State<ItemListView> {
+  final List<Item> items = [Item(name: "Bill 1")];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Budget Buddy"),
+      ),
+      body: ListView.builder(
+        restorationId: 'sampleItemListView',
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = items[index];
+
+          return ListTile(
+              title: Text(item.name),
+              leading: const CircleAvatar(
+                // This might be the picture of the item
+                backgroundColor: Colors.green,
+              ),
+              onTap: () {
+                //use navigator to open up a details dialog box.
+              });
+        },
+      ),
+    );
+  }
+}
---
+++ budget_buddy/lib/objects/item.dart
@@ -0,0 +1,5 @@
+class Item {
+  Item({required this.name});
+
+  final String name;
+}
